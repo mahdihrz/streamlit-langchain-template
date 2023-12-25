c3b30242a96b8e126aa1f51f24bb631b28ccc798
mahdihrz <mahdihrz@gmail.com>
2023-12-25 18:12:48 +0100

    add first interaction page

--- app/app.py
+++ app/app.py
@@ -1,8 +1,15 @@
 import streamlit as st
 
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage, SystemMessage
+
 # Setup variables
 # os.environ['LANGCHAIN_TRACING_V2'] = st.secrets["openai_api_key"]["tracing"]
+#-----------------------------------------------------------------------------------------------------------
 
+
+
+#------------------------------------------------------------------------------------------------------------
 st.set_page_config(
     page_title="Langchain-demo",
     page_icon="📝",
@@ -27,18 +34,34 @@ def work_page():
     import streamlit as st
     st.title("Streamlit & Langchain template 📝")
     st.caption("Using the power of LLMs")
-    question = st.text_area("Research question", placeholder="Question "
-                            "to be  "
-                            "asked?")
+    systemMessageQuestion = st.text_area("SystemMessage", value ="You are a helpful python, langchain and openai programmer.")
+    humanMessageQuestion = st.text_area("HumanMessage", value ="Suggest some basic ideas of usage of langchain and openai and generate their code.")
 
     if 'openai_api_key' not in st.session_state:
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
-    if not question:
-        st.info("Please enter a question to continue ...")
+    if not systemMessageQuestion:
+        st.info("Please enter a SystemMessage to continue ...")
+    if not humanMessageQuestion:
+        st.info("Please enter a MessageQuestion to continue ...")        
     elif st.button("🚄 Perform Request"):
         # Do what to do
         st.info("Performing")
+        chat = ChatOpenAI(temperature=st.session_state["temperature"], model_name=st.session_state['model_to_use'], openai_api_key=st.session_state["openai_api_key"])
+
+        messages = [
+                    SystemMessage(
+                        content=systemMessageQuestion
+                    ),
+                    HumanMessage(
+                        content=humanMessageQuestion
+                    ),
+                ]
+
+        # Add a message and get a response
+        response=chat(messages)
+
+        st.markdown(response.content)
     st.stop()
 
 
@@ -73,7 +96,7 @@ with st.sidebar:
         "Work": work_page,
         "About": about_demo,
     }
-    demo_name = st.sidebar.selectbox("Choose a demo", page_names_to_funcs.keys())
+    demo_name = st.sidebar.selectbox("Choose a demo", page_names_to_funcs.keys(), index=0)
     
     st.divider()
     st.markdown("Made with ❤️ by [Mahdi HARZALLAH](https://www.linkedin.com/in/mahdi-harzallah/)")
