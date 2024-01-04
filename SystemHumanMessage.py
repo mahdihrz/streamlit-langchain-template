57d17056c51976f6dab05935e0308011e79011fe
mahdihrz <mahdihrz@gmail.com>
2024-01-04 11:32:26 +0100

    format code & externalize pages

---
+++ SystemHumanMessage.py
@@ -0,0 +1,63 @@
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage, SystemMessage
+
+def apply_template():
+    match st.session_state['templateSystem'] :
+        case "Langchain programmer":
+            st.session_state['systemMessageQuestion']= "You are a helpful python, langchain and openai programmer."
+            st.session_state['HumanMessageQuestion']= "Suggest some basic ideas of usage of langchain and openai and generate their code."
+        case "Java programmer":
+            st.session_state['systemMessageQuestion'] = "You are a helpful java programmer."
+            st.session_state['HumanMessageQuestion']= "Suggest some basic ideas of usage of Java and Spring framework, and generate their code."
+        case "Expo.io & React Native programmer":
+            st.session_state['systemMessageQuestion'] = "You are a helpful Expo.io and React Native programmer."
+            st.session_state['HumanMessageQuestion']= "Suggest some basic ideas of usage of Expo.io framework, and generate their code."
+        case "Travel Planner":
+            st.session_state['systemMessageQuestion'] = "You are a helpful Travel Planner."
+            st.session_state['HumanMessageQuestion']= "I am traveling to Bali suggest a plan for visit."
+
+def work_page():
+    import streamlit as st
+    st.title("Streamlit & Langchain template 📝")
+    st.caption("Using the power of LLMs")
+    
+    st.session_state['templateSystem'] = st.selectbox("Select System Message Template", ["Langchain programmer", "Java programmer", "Expo.io & React Native programmer", "Travel Planner"], index=0)
+    apply_template()
+
+    systemMessageQuestion = st.text_area("SystemMessage", value =st.session_state['systemMessageQuestion'])
+    humanMessageQuestion = st.text_area("HumanMessage", value =st.session_state['HumanMessageQuestion'])
+
+    if 'openai_api_key' not in st.session_state:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+    if not systemMessageQuestion:
+        st.info("Please enter a SystemMessage to continue ...")
+    if not humanMessageQuestion:
+        st.info("Please enter a MessageQuestion to continue ...")        
+    elif st.button("🚄 Perform Request"):
+        # Do what to do
+        st.info("Performing")
+        chat = ChatOpenAI(temperature=st.session_state["temperature"], model_name=st.session_state['model_to_use'], openai_api_key=st.session_state["openai_api_key"])
+
+        messages = [
+                    SystemMessage(
+                        content=systemMessageQuestion
+                    ),
+                    HumanMessage(
+                        content=humanMessageQuestion
+                    ),
+                ]
+
+        # Add a message and get a response
+        response=chat(messages)
+        st.session_state["response"] = response.content
+
+        st.markdown(st.session_state["response"])
+        if st.session_state["response"]:
+            st.download_button(
+            label="Download the last response",
+            data=st.session_state["response"],
+            file_name='report.md',
+            mime='text/markdown',)
+    st.stop()
--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 import streamlit as st
 
-from langchain.chat_models import ChatOpenAI
-from langchain.schema import HumanMessage, SystemMessage
-
+from SystemHumanMessage import work_page
 # Setup variables
 # os.environ['LANGCHAIN_TRACING_V2'] = st.secrets["openai_api_key"]["tracing"]
-#-----------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------
 
-
-
-#------------------------------------------------------------------------------------------------------------
 st.set_page_config(
     page_title="Langchain-demo",
     page_icon="📝",
@@ -21,59 +16,19 @@ st.set_page_config(
     }
 )
 
-
-## Pages
-
 def about_demo():
     import streamlit as st
     st.markdown(open("README.md", "r").read())
     st.stop()
 
-
-def work_page():
-    import streamlit as st
-    st.title("Streamlit & Langchain template 📝")
-    st.caption("Using the power of LLMs")
-    systemMessageQuestion = st.text_area("SystemMessage", value ="You are a helpful python, langchain and openai programmer.")
-    humanMessageQuestion = st.text_area("HumanMessage", value ="Suggest some basic ideas of usage of langchain and openai and generate their code.")
-
-    if 'openai_api_key' not in st.session_state:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
-    if not systemMessageQuestion:
-        st.info("Please enter a SystemMessage to continue ...")
-    if not humanMessageQuestion:
-        st.info("Please enter a MessageQuestion to continue ...")        
-    elif st.button("🚄 Perform Request"):
-        # Do what to do
-        st.info("Performing")
-        chat = ChatOpenAI(temperature=st.session_state["temperature"], model_name=st.session_state['model_to_use'], openai_api_key=st.session_state["openai_api_key"])
-
-        messages = [
-                    SystemMessage(
-                        content=systemMessageQuestion
-                    ),
-                    HumanMessage(
-                        content=humanMessageQuestion
-                    ),
-                ]
-
-        # Add a message and get a response
-        response=chat(messages)
-
-        st.markdown(response.content)
-    st.stop()
-
-
-### Main Code
-    
 with st.sidebar:
     st.title("Getting started")
 
     if ("openai_api_key" not in st.secrets or
         "openai_api_key" in st.secrets and
-        st.secrets["openai_api_key"] == "your key here"):
-        openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+            st.secrets["openai_api_key"] == "your key here"):
+        openai_api_key = st.text_input(
+            "OpenAI API Key", key="chatbot_api_key", type="password")
         if openai_api_key and 'openai_api_key' not in st.session_state:
             st.session_state['openai_api_key'] = openai_api_key
         "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
@@ -85,21 +40,25 @@ with st.sidebar:
     options = st.container()
     options.title("Options ⚙️")
 
-    model_to_use = options.selectbox("Select model", ["gpt-3.5-turbo-16k", "gpt-3.5-turbo-1106", "gpt-4-32k-0613", "gpt-4-1106-preview"], index=1)
+    model_to_use = options.selectbox("Select model", [
+                                     "gpt-3.5-turbo-16k", "gpt-3.5-turbo-1106", "gpt-4-32k-0613", "gpt-4-1106-preview"], index=1)
 
     st.session_state['model_to_use'] = model_to_use
-    temperature = options.slider('Model Temperature?',min_value=0.0,     max_value=1.0,     step=0.1,     value=0.7)
+    temperature = options.slider(
+        'Model Temperature?', min_value=0.0,     max_value=1.0,     step=0.1,     value=0.7)
     st.session_state['temperature'] = temperature
     st.divider()
 
-    ## -------- Pages  ----------
+    # -------- Pages  ----------
     page_names_to_funcs = {
         "Work": work_page,
         "About": about_demo,
     }
-    demo_name = st.sidebar.selectbox("Choose a demo", page_names_to_funcs.keys(), index=0)
-    
+    demo_name = st.sidebar.selectbox(
+        "Choose a demo", page_names_to_funcs.keys(), index=0)
+
     st.divider()
-    st.markdown("Made with ❤️ by [Mahdi HARZALLAH](https://www.linkedin.com/in/mahdi-harzallah/)")
+    st.markdown(
+        "Made with ❤️ by [Mahdi HARZALLAH](https://www.linkedin.com/in/mahdi-harzallah/)")
 
 page_names_to_funcs[demo_name]()
